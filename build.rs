1c06f2fd908ed70bb79b6ff81178399cc2da410d
VladislavPim <pimenovvlad895@gmail.com>
2026-03-13 21:06:32 +0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -3,7 +3,22 @@ extern crate winres;
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
+        
+        // Иконка
         res.set_icon("assets/logo.ico");
+        
+        res.set("FileVersion", "1.0.0.0");
+        res.set("ProductVersion", "1.0.0.0");
+        res.set("CompanyName", "VladislavPim");
+        res.set("FileDescription", "Control Terminal");
+        res.set("ProductName", "ControlTerminal");
+        res.set("LegalCopyright", "© 2026 VladislavPim");
+        res.set("OriginalFilename", "ControlTerminal.exe");
+        res.set("InternalName", "ControlTerminal");
+        
+        // Язык (английский США)
+        res.set_language(0x0409);
+        
         res.compile().unwrap();
     }
 }
